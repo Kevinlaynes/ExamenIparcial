29efc9cc609be2d36c98847e77652f4ca5f7c855
Kevinlaynes <101679413+Kevinlaynes@users.noreply.github.com>
2022-05-06 22:23:54 -0600

    Add files via upload

---
+++ AppCore/IServices/IService.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCore.Iservices
+{
+   public interface IService
+    {
+
+        Task<WheaterData.ForeCastInfo> GetWeatherForeCast(double Lat, double Lot, long dt);
+        Root GetWeather(string ciudad);
+        string GetImageLocation(wheather w);
+
+        DateTime convertToDateTime(long milisegundos);
+
+    }
+}
